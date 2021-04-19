38bfa66f360c85ad3481c741d9bb23a3d736ad1f
Callum Ilett <cilett99@gmail.com>
2021-04-19 23:22:42 +0100

    Made homepage use dynamic data

---
+++ src/pages/api/playlists/discover.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { firestore } from "../../../lib/firebase";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const discover = firestore.collection("playlists").doc("discover");
+
+  try {
+    const { docs } = await discover.collection("songs").get();
+
+    const songs = docs.map((document) => document.data());
+
+    res.status(200).json({ name: "Discover", songs });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
