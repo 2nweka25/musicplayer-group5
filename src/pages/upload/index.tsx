5cb0c68a17b099e80d07606bc51f6cba0631acd8
Krisztian Nagy <k.nagy.k98@gmail.com>
2021-04-21 20:59:00 +0100

    initial upload

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -5,13 +5,36 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import React, { useState, useContext } from "react";
 import Artwork from "../../components/artwork";
 import Navbar from "../../components/navbar";
+import uploadService from "../../lib/services/upload";
+import getProfile from "../../lib/services/user";
+import AuthContext from "../../lib/authContext"
 
-interface Props {}
+interface Props { }
 
 const Upload = (props: Props) => {
+  const user = useContext(AuthContext)
+
+  const [data, setData] = useState({ title: "", description: "", songUrl: "", imgUrl: "/images/artwork/1.jpg", tags: "" })
+
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    /*
+    if (user) {
+      const upload = await uploadService.uploadHandle(data.songUrl, user.userId)
+
+    }*/
+    const upload = await uploadService.uploadImage(data.imgUrl)
+    console.log(upload)
+  }
+
   return (
     <div>
       <Navbar />
@@ -22,16 +45,19 @@ const Upload = (props: Props) => {
           <Artwork src="/images/artwork/1.jpg" />
         </Box>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField
+            name="title"
             variant="outlined"
             label="Song Name"
             color="primary"
             fullWidth
             margin="normal"
+            onChange={handleInput}
           />
 
           <TextField
+            name="description"
             variant="outlined"
             label="Song Description"
             multiline
@@ -39,22 +65,27 @@ const Upload = (props: Props) => {
             color="primary"
             fullWidth
             margin="normal"
+            onChange={handleInput}
           />
 
           <TextField
+            name="songUrl"
             variant="outlined"
             label="Upload Link"
             color="primary"
             fullWidth
             margin="normal"
+            onChange={handleInput}
           />
 
           <TextField
+            name="tags"
             variant="outlined"
             label="Hashtags"
             color="primary"
             fullWidth
             margin="normal"
+            onChange={handleInput}
           />
 
           <Box mt={2} width="70%" margin="auto">
