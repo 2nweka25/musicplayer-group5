3ea7ee822d768c63cc30c7e3ee49dfddaf47c369
Callum Ilett <cilett99@gmail.com>
2021-04-28 17:01:04 +0100

    Added relevant meta tags to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,7 +41,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title>My page</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
       <ThemeProvider theme={theme}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,34 @@ import React from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 
+const APP_NAME = "SolTunes";
+const APP_DESCRIPTION = "Social media player app";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
